c7466de5c83dc4d10f92206a8d8531c1b6e2f73a
Vinícius Lavrador <vinicius.lavrador@icloud.com>
2022-04-19 03:54:39 -0300

    expanded use of config service

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,16 +1,21 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigInterface } from 'src/config';
 import { User, UserDocument } from '../entities/auth.entity';
 import { JwtService } from '../services/jwt.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(@Inject(JwtService) private readonly jwtService: JwtService) {
+  constructor(
+    @Inject(JwtService) private readonly jwtService: JwtService,
+    @Inject(ConfigService) private readonly configService: ConfigService<ConfigInterface>,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'dev',
-      ignoreExpiration: true,
+      secretOrKey: configService.get('JWT_SECRET', { infer: true }),
+      ignoreExpiration: false,
     });
   }
 
