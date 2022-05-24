39da3c82784ea924f44d1c061e0335bb2361c45b
Vinícius Lavrador <vinicius.lavrador@icloud.com>
2022-05-24 02:51:20 -0300

    Improved transform enum decorator

--- src/common/decorators/transform-role.decorator.ts
+++ src/common/decorators/transform-role.decorator.ts
@@ -2,4 +2,6 @@ import { Transform } from 'class-transformer';
 import { Role as InternalRoleEnum } from 'src/auth/entities/role.enum';
 import { Role as ProtoRoleEnum } from 'src/auth/dto/proto/auth.pb';
 
-export const TransformRoleEnum = () => Transform(({ value }) => InternalRoleEnum[ProtoRoleEnum[value]]);
+const TransformEnums = <T, G>(OuterEnum: T, InnerEnum: G) => Transform(({ value }) => OuterEnum[InnerEnum[value]]);
+
+export const TransformRoleEnum = () => TransformEnums(InternalRoleEnum, ProtoRoleEnum);
